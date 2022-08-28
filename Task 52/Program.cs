b518bb6d4bc8dd1bf11a93c3d85d566cdba930ed
Catherine <kankatya@bk.ru>
2022-08-28 16:41:41 +0500

    Fixed task 52

--- Task 52/Program.cs
+++ Task 52/Program.cs
@@ -19,14 +19,14 @@
 }
 void FillArray(int[,]arr)
 {
-     for(int i = 0;i<arr.GetLength(0);i++)
-     {
-          for(int j =0;j<arr.GetLength(1); j++)
-          {
-                arr[i,j] = new Random().Next(0, 10);
-           }
-       }
-       Console.WriteLine();
+    for(int i = 0;i<arr.GetLength(0);i++)
+    {
+        for(int j =0;j<arr.GetLength(1); j++)
+        {
+           arr[i,j] = new Random().Next(0, 10);
+        }
+    }
+    Console.WriteLine();
 }
 void FindMean(int[,]arr)
 {
